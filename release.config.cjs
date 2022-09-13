ec25e48020c3ea1dee6bb8651e899a347f9aa385
carlo <carlo.dambrosio@lovetoknow.com>
2022-09-13 10:46:35 +0200

    feat(release): add initial setup for semantic releases

---
+++ release.config.cjs
@@ -0,0 +1,21 @@
+/* eslint-disable no-template-curly-in-string */
+module.exports = {
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md',
+        },
+      ],
+      [
+        '@semantic-release/npm',
+        {
+          npmPublish: false,
+        },
+      ],
+      '@semantic-release/github'      ,
+    ],
+  }
