67d0e2011179e547e8dbe931851d93be4b21f5e4
carlo <carlo.dambrosio@lovetoknow.com>
2022-09-13 10:53:19 +0200

    refactor(release): try out removing changelong plugin

--- release.config.cjs
+++ release.config.cjs
@@ -4,12 +4,12 @@ module.exports = {
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
-      [
+      /* [
         '@semantic-release/changelog',
         {
           changelogFile: 'CHANGELOG.md',
         },
-      ],
+      ],*/
       '@semantic-release/github',
     ],
   }
